772231cf719ba4a23829d2c4b0af909ba89ae0b3
Yazan Alqasem <100817706+Tufaah@users.noreply.github.com>
2022-03-31 12:51:35 -1200

    Update Yahoo.py

--- search.yahoo.com/Yahoo.py
+++ search.yahoo.com/Yahoo.py
@@ -5,8 +5,7 @@ from bs4 import BeautifulSoup
 def yahoo(search :str, page :int):
     links, websites, titles, descriptions, data, done = [], [], [], [], [], []
     res = get(f'https://search.yahoo.com/search?p={search}&fr=sfp&fr2=p:s,v:sfp,m:sb-top&b={page if page == 1 else page + 7}',headers={
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'
-    }).content
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'}).content
 
     soup = BeautifulSoup(res, "lxml")
     link = soup.find_all("a", {"class":"d-ib ls-05 fz-20 lh-26 td-hu tc va-bot mxw-100p"})
@@ -19,10 +18,10 @@ def yahoo(search :str, page :int):
 
     description = soup.find_all("span", {"class":"fc-falcon"})
     for i in range(len(description)): descriptions.append(str(description[i].text))
-    for y in range(len(websites)): data.append(str(titles[y]).replace(str(websites[y]),''))
+    for j in range(len(websites)): data.append(str(titles[j]).replace(str(websites[j]),''))
 
-    for x in range(len(links)): done.append({'link':str(links[x]), 'title':str(data[x]), 'website':str(websites[x]).split()[0], 'description':str(descriptions[x]), })
-    print({'results':0, 'data':f'Cant find results for: {search}'} if not done else {'results':len(links), 'data':done})
+    for i in range(len(links)): done.append({'link':str(links[i]), 'title':str(data[i]), 'website':str(websites[i]).split()[0], 'description':str(descriptions[i]), })
+    return {'results':0, 'data':f'Cant find results for: {search}'} if not done else {'results':len(links), 'data':done}
 
 
-yahoo('abc', 1)
+print(yahoo('python', 1))
